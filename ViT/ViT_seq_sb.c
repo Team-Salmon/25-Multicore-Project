63dd7145b8d344b4de32853a71ece5422741e47a
Seungbin <seungbin0619@naver.com>
2025-12-02 22:24:38 +0900

    test

--- ViT/ViT_seq_sb.c
+++ ViT/ViT_seq_sb.c
@@ -28,8 +28,8 @@
 #define li_lws_token 64
 #define li_tpt 4 // tokens per thread
 #define li_opt 8 // output per thread
-#define li_tile 16
-#define li_stride_in 17
+#define li_tile 32
+#define li_stride_in 33
 #define li_stride_weight 33
 
 #define dfl_ls 256 // default local size
