77d18c514d8b0be9792481968fe13b143ac0a166
Seungbin <seungbin0619@naver.com>
2025-12-03 22:28:06 +0900

    Update Main.c

--- ViT/Main.c
+++ ViT/Main.c
@@ -46,8 +46,8 @@ int main() {
     printf("=====================Start========================\n");
     start = clock();
     // Input here - 병렬처리 함수 작성
-	ViT_seq(images, network, probabilities);
-	// ViT_seq_sb(images, network, probabilities);
+	// ViT_seq(images, network, probabilities);
+	ViT_seq_sb(images, network, probabilities);
 	// ViT_seq_seoh(images, network, probabilities);
 	// ViT_seq_ZZani(images, network, probabilities);
     
